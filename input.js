f75a1a88cd06d5a34e7382851aca191bbc6ca0b2
JoshLampen <joshlampen2@gmail.com>
2020-04-22 17:59:18 +0000

    initializing html and css, getting started on js input

---
+++ input.js
@@ -0,0 +1,11 @@
+let chartElements = {}
+let chartOptions = {}
+
+function drawBarChart(data) {
+  for (number of data) {
+    let element = 'element' + number.toString();
+    return element;
+  }
+};
+
+drawBarChart([1, 2, 3]);
