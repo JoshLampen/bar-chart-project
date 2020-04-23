46a0c413c2ffc3223cc2166de765917671d52ed8
JoshLampen <joshlampen2@gmail.com>
2020-04-23 19:08:05 +0000

    Figured out base template for linking JS command to HTML doc

--- input.js
+++ input.js
@@ -1,11 +1,24 @@
-let chartElements = {}
-let chartOptions = {}
+const elementHeights = [];
+const chartOptions = {};
 
 function drawBarChart(data) {
-  for (number of data) {
-    let element = 'element' + number.toString();
-    return element;
+  for (let i = 0; i < data.length; i++) {
+    let object = {};
+    object.height = data[i];
+    elementHeights.push(object);
   }
+  return elementHeights;
 };
 
-drawBarChart([1, 2, 3]);
+drawBarChart([95, 80, 90]);
+
+function makeBar(bar) {
+  return `
+  <div style="background-color:#ff4136; border-radius:5px 5px 0 0; grid-row-start:${101 - bar.height}; grid-row-end:101;">
+  </div>
+  `
+}
+
+document.getElementById("chart").innerHTML = `
+${elementHeights.map(makeBar).join('')}
+`
